e930366ccc15b9097c8660c60644e1d595d93cdd
Rifat Sakib <sakibrifat353@gmail.com>
2023-11-25 21:07:01 +0600

    second commit

--- src/Components/Home/Home/Home.js
+++ src/Components/Home/Home/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from '../Header/Header';
 import Services from '../Services/Services';
+import FeatureService from '../FeatureService/FeatureService';
+import MakeAppointment from '../MakeAppointment/MakeAppointment';
 
 const Home = () => {
     return (
         <div className='container-fluid'>
           <Header></Header>
           <Services></Services>
+          <FeatureService></FeatureService>
+          <MakeAppointment></MakeAppointment>
         </div>
     );
 };
